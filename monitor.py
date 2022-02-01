a504e2a1ba081a30d5c8e0127d2bfa51705c7202
Pawel Granat <pgranat@gmail.com>
2022-02-01 20:52:00 +0100

    Update and change of functionality

--- monitor.py
+++ monitor.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright (c) 2017 Adafruit Industries
 # Author: Tony DiCola & James DeVito
 #
@@ -18,66 +19,49 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-import time
+# Copyright (c) 2021 Pawel Granat
+# Changed:
+# Upgraded to python3
+# Upgraged to circuitpython oled driver instead of old one (deprecated)
+# Added weather - current and forecast
 
-import Adafruit_GPIO.SPI as SPI
-import Adafruit_SSD1306
 
+
+
+    
+# Import all board pins.
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
-
 import subprocess
+import time
+from pyowm import OWM
+from pyowm.utils import config
+from pyowm.utils import timestamps
 
-# Raspberry Pi pin configuration:
-RST = None     # on the PiOLED this pin isnt used
-# Note the following are only used with SPI:
-DC = 23
-SPI_PORT = 0
-SPI_DEVICE = 0
 
-# Beaglebone Black pin configuration:
-# RST = 'P9_12'
-# Note the following are only used with SPI:
-# DC = 'P9_15'
-# SPI_PORT = 1
-# SPI_DEVICE = 0
+# i2c = busio.I2C(board.SCL1, board.SDA1)  # QT Py RP2040 STEMMA connector
+# i2c = busio.I2C(board.GP1, board.GP0)    # Pi Pico RP2040
 
-# 128x32 display with hardware I2C:
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
+# Import the SSD1306 module.
+import adafruit_ssd1306
+from adafruit_extended_bus import ExtendedI2C as i2c
 
-# 128x64 display with hardware I2C:
-disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
+# Create the I2C interface.
+# Selectable i2c bus
+i2c = i2c(5)
 
-# Note you can change the I2C address by passing an i2c_address parameter like:
-# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3C)
+# Create the SSD1306 OLED class.
+# The first two parameters are the pixel width and pixel height.  Change these
+# to the right size for your display!
+display = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c)
 
-# Alternatively you can specify an explicit I2C bus number, for example
-# with the 128x32 display you would use:
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, i2c_bus=2)
 
-# 128x32 display with hardware SPI:
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
-
-# 128x64 display with hardware SPI:
-# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
-
-# Alternatively you can specify a software SPI implementation by providing
-# digital GPIO pin numbers for all the required display pins.  For example
-# on a Raspberry Pi with the 128x32 display you might use:
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, sclk=18, din=25, cs=22)
-
-# Initialize library.
-disp.begin()
-
-# Clear display.
-disp.clear()
-disp.display()
 
 # Create blank image for drawing.
 # Make sure to create image with mode '1' for 1-bit color.
-width = disp.width
-height = disp.height
+width = display.width
+height = display.height
 image = Image.new('1', (width, height))
 
 # Get drawing object to draw on image.
@@ -101,57 +85,131 @@ font = ImageFont.load_default()
 # Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
 # Some other nice fonts to try: http://www.dafont.com/bitmap.php
 font = ImageFont.truetype('Montserrat-Light.ttf', 12)
-font2 = ImageFont.truetype('fontawesome-webfont.ttf', 14)
-font_icon = ImageFont.truetype('fontawesome-webfont.ttf', 20)
+# changed to newer
+font2 = ImageFont.truetype('fa-solid-900.ttf', 14)
+font_icon = ImageFont.truetype('fa-solid-900.ttf', 20)
 font_text_small = ImageFont.truetype('Montserrat-Medium.ttf', 8)
 
-while True:
+start_time = time.time()
+# Forecast download interval
+secondsday = 86400
+# Current weather download interval
+secondshour = 3600
 
+
+
+#Forecast - icons dependent on 
+def forecast():
+    owm = OWM('your')
+    mgr = owm.weather_manager()
+    one_call = mgr.one_call(lat=52.185942, lon=20.990242)
+    curcond = one_call.forecast_daily[0].status
+    if curcond == "Snow":
+      foricon=chr(62172) # OK
+    elif curcond == "Thunderstorm":
+      foricon=chr(61671) # OK
+    elif curcond == "Drizzle":
+      foricon=chr(63293) # OK
+    elif curcond == "Rain":
+      foricon=chr(63293) # OK
+    elif curcond == "Mist":
+      foricon=chr(63327)   # OK
+    elif curcond == "Smoke":
+      foricon=chr(63327)  # OK
+    elif curcond == "Haze":
+      foricon=chr(63327)  # OK
+    elif curcond == "Dust":
+      foricon=chr(63327) # OK
+    elif curcond == "Fog":
+      foricon=chr(63327)   #OK
+    elif curcond == "Sand":
+     foricon=chr(63327) #OK
+    elif curcond == "Dust":
+      foricon=chr(63327) # OK
+    elif curcond == "Ash": 
+      foricon=chr(63327) # OK
+    elif curcond == "Squall":
+      foricon=chr(63278) # OK
+    elif curcond == "Tornado":
+      foricon=chr(63278) # OK
+    elif curcond == "Clear":
+      foricon=chr(61829)
+    elif curcond == "Clouds":
+      foricon=chr(61634)
+    else:
+      foricon=chr(61953)
+      
+    fortmp = one_call.forecast_daily[0].temperature('celsius').get('day', None) 
+    fortmp = str(fortmp)+" C"
+    return fortmp,foricon
+
+#Current weather
+def current_weather():
+    owm = OWM('yourapikey')
+    mgr = owm.weather_manager()
+    one_call = mgr.one_call(lat=52.185942, lon=20.990242)
+    outtmp=one_call.current.temperature('celsius').get('temp', None)
+    outtmp=str(outtmp)+" C"
+    outhum=one_call.current.humidity
+    outhum=str(outhum)+" %"
+    return outtmp, outhum
+
+fortmp,foricon = forecast()
+outtmp, outhum = current_weather()
+
+while True:
+    current_time = time.time()
+    elapsed_time = current_time - start_time
+    
+    #Download forecast data once per day
+    if elapsed_time > secondsday:
+        fortmp,foricon = forecast()
+    #Download current temp once per hour
+    if elapsed_time > secondshour:
+        outtmp, outhum = current_weather()
+        
+        
+        
+    
     # Draw a black filled box to clear the image.
     draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-    # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
-    cmd = "hostname -I | cut -d\' \' -f1 | head --bytes -1"
-    IP = subprocess.check_output(cmd, shell = True )
-
-    cmd = "top -bn1 | grep load | awk '{printf \"CPU %.2f\", $(NF-2)}'"
-    CPU = subprocess.check_output(cmd, shell = True )
     
-    cmd = "free -m | awk 'NR==2{printf \"%.2f%%\", $3*100/$2 }'"
-    MemUsage = subprocess.check_output(cmd, shell = True )
-    
-    cmd = "df -h | awk '$NF==\"/\"{printf \"HDD: %d/%dGB %s\", $3,$2,$5}'"
-    cmd = "df -h | awk '$NF==\"/\"{printf \"%s\", $5}'"
-    Disk = subprocess.check_output(cmd, shell = True )
-    
-    cmd = "vcgencmd measure_temp | cut -d '=' -f 2 | head --bytes -1"
+    cmd = "vcgencmd measure_temp | cut -d '=' -f 2 | head --bytes -3"
     Temperature = subprocess.check_output(cmd, shell = True )
-
+    Temperature = Temperature.decode('utf-8')
+    Temperature=str(Temperature)+" C"
+    
     # Icons
-    # Icon temperator
-    draw.text((x, top+5),    unichr(62152),  font=font_icon, fill=255)
-    # Icon memory
-    draw.text((x+60, top+5), unichr(62171),  font=font_icon, fill=255)
-    # Icon disk
-    draw.text((x, top+30), unichr(61888),  font=font2, fill=255)
-    # Icon Wifi
-    draw.text((x, top+52), unichr(61931),  font=font2, fill=255)
+    # Icon temperature -> 1 column up -> Current temperature
+    draw.text((x, top+5),    chr(62152),  font=font_icon, fill=255)
+    
+    # Icon dependent on forecast -> 2 column up -> Weather forecast
+    draw.text((x+60, top+5), foricon,  font=font_icon, fill=255)
+    
+    # Icon droplet -> 1 column down -> Current humidity
+    draw.text((x, top+30), chr(62657),  font=font2, fill=255)
+    
+    # Icon Computer -> 2 column down -> Temp CPU 
+    draw.text((x+60, top+30), chr(63231),  font=font2, fill=255)
+    
+    # Icon Wifi -> Sam dol
+    #draw.text((x, top+52), chr(61931),  font=font2, fill=255)
        
-  # Text temperatur 
-    draw.text((x+15, top+5), str(Temperature),  font=font, fill=255)
-  # Text mem usage  
-    draw.text((x+80, top+5),    str(MemUsage),  font=font, fill=255)
-  # Text Disk usage  
-    draw.text((x+15, top+30),   str(Disk),  font=font, fill=255)
+  # Text temperature 
+    draw.text((x+15, top+5), outtmp,  font=font, fill=255)
+  # Text forecsat
+    draw.text((x+80, top+5),    fortmp,  font=font, fill=255)
+  # Text humidity
+    draw.text((x+20, top+30),   outhum,  font=font, fill=255)
   # Text cpu usage  
-    draw.text((x+60, top+30),      str(CPU), font=font, fill=255)
+    draw.text((x+80, top+30),      Temperature, font=font, fill=255)
   
    # Text IP addresss  
-    draw.text((x+20, top+55),      str(IP),  font=font_text_small, fill=255)
+    #draw.text((x+20, top+55),      str(IP),  font=font_text_small, fill=255)
     
    # Display image.
-    disp.image(image)
-    disp.display()
+
+    display.image(image)
+    display.show()
     time.sleep(5)
-
-
